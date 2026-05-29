d3d977c705cad552f464fe453d23b78f4701d290
Bruno Ioneda <brunohideoioneda@gmail.com>
2026-05-29 17:57:22 -0300

    Adiciona controller para autenticação

---
+++ src/VaccinationCard.API/Controllers/AuthController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using VaccinationCard.API.Contracts;
+using VaccinationCard.Application.Common.Interfaces;
+
+namespace VaccinationCard.API.Controllers;
+
+[ApiController]
+[AllowAnonymous]
+[Route("api/auth")]
+public class AuthController(IJwtTokenGenerator jwtTokenGenerator) : ControllerBase
+{
+    private readonly IJwtTokenGenerator _jwtTokenGenerator = jwtTokenGenerator;
+
+    [HttpPost("login")]
+    public IActionResult Login([FromBody] LoginRequest request)
+    {
+        if (request.UserName != "admin" || request.Password != "admin")
+        {
+            return Unauthorized(new { title = "Credenciais inválidas." });
+        }
+
+        string token = _jwtTokenGenerator.GenerateToken(
+            userId: Guid.NewGuid().ToString(),
+            userName: request.UserName);
+
+        return Ok(new { accessToken = token, tokenType = "Bearer" });
+    }
+}
