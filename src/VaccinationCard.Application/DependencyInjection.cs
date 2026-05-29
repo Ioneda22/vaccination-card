13c831d70bcff95bb7bb004bebd84500089e7018
Bruno Ioneda <brunohideoioneda@gmail.com>
2026-05-29 18:18:22 -0300

    Adiciona Injeção de dependência em Application

---
+++ src/VaccinationCard.Application/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using VaccinationCard.Application.Common.Behaviors;
+
+namespace VaccinationCard.Application;
+
+// Registro dos serviços da camada de Application (MediatR, validadores e pipeline de validação).
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        Assembly assembly = typeof(DependencyInjection).Assembly;
+
+        services.AddMediatR(configuration =>
+            configuration.RegisterServicesFromAssembly(assembly));
+
+        services.AddValidatorsFromAssembly(assembly);
+
+        // O pipeline de validação roda os validadores do FluentValidation antes de cada handler.
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+
+        return services;
+    }
+}
