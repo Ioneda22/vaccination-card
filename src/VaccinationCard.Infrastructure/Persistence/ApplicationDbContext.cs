0c48c9eb88e8b10de2abaebd1a932a57c3844267
Bruno Ioneda <brunohideoioneda@gmail.com>
2026-05-29 16:52:57 -0300

    Adiciona DBContext e UnitOfWork

---
+++ src/VaccinationCard.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VaccinationCard.Domain.Entities;
+
+namespace VaccinationCard.Infrastructure.Persistence;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+{
+    public DbSet<Person> People => Set<Person>();
+    public DbSet<Vaccine> Vaccines => Set<Vaccine>();
+    public DbSet<VaccinationRecord> VaccinationRecords => Set<VaccinationRecord>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        base.OnModelCreating(modelBuilder);
+    }
+}
---
+++ src/VaccinationCard.Infrastructure/Persistence/UnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using VaccinationCard.Application.Common.Interfaces;
+
+namespace VaccinationCard.Infrastructure.Persistence;
+
+public class UnitOfWork(ApplicationDbContext context) : IUnitOfWork
+{
+    private readonly ApplicationDbContext _context = context;
+
+    public Task<int> SaveChangesAsync(CancellationToken cancellationToken = default) =>
+        _context.SaveChangesAsync(cancellationToken);
+}
