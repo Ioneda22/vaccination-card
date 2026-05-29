9dd092d3be6905ac282cb81b2469f0227a9a120e
Bruno Ioneda <brunohideoioneda@gmail.com>
2026-05-29 17:19:34 -0300

    Adiciona injeção de dependência

--- src/VaccinationCard.Infrastructure/DependencyInjection.cs
+++ src/VaccinationCard.Infrastructure/DependencyInjection.cs
@@ -1,1 +1,27 @@
-﻿
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using VaccinationCard.Application.Common.Interfaces;
+using VaccinationCard.Infrastructure.Persistence;
+using VaccinationCard.Infrastructure.Persistence.Repositories;
+
+namespace VaccinationCard.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseInMemoryDatabase("VaccinationCardDb"));
+
+        services.AddScoped<IPersonRepository, PersonRepository>();
+        services.AddScoped<IVaccineRepository, VaccineRepository>();
+        services.AddScoped<IVaccinationRecordRepository, VaccinationRecordRepository>();
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+        return services;
+    }
+}
