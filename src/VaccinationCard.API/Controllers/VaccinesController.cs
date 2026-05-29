9b1c56ee9fde868c126155c214ab6b05cd70f351
Bruno Ioneda <brunohideoioneda@gmail.com>
2026-05-29 17:52:41 -0300

    Adiciona Controller para vacinas

---
+++ src/VaccinationCard.API/Controllers/VaccinesController.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using VaccinationCard.API.Contracts;
+using VaccinationCard.Application.Features.Vaccines.Commands.CreateVaccine;
+
+namespace VaccinationCard.API.Controllers;
+
+[ApiController]
+[Authorize]
+[Route("api/vaccines")]
+public class VaccinesController(ISender sender) : ControllerBase
+{
+    private readonly ISender _sender = sender;
+
+    [HttpPost]
+    public async Task<IActionResult> Create(
+        [FromBody] CreateVaccineRequest request,
+        CancellationToken cancellationToken)
+    {
+        Guid vaccineId = await _sender.Send(new CreateVaccineCommand(request.Name), cancellationToken);
+        return Created($"/api/vaccines/{vaccineId}", new { id = vaccineId });
+    }
+}
